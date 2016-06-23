6eeb04abac62a7d315daa4292e1a10baac1b955f
huayun321 <huayun321@gmail.com>
2016-06-24 04:48:24 +0800

    cin

---
+++ cpp-001-myfirst.cpp
@@ -0,0 +1,14 @@
+//myfirst.cpp--displays a messages
+
+#include <iostream>  //PREPROCESSOR directive
+
+int main() 
+{
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+
+    cout << "You won't regret it!" << endl;
+    cin.get();
+    return 0;
+}
