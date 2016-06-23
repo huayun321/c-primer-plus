3e8e6eee5219c027001979394c29729fbeca93b8
huayun321 <huayun321@gmail.com>
2016-06-24 03:39:08 +0800

    my first cpp

---
+++ myfirst.cpp
@@ -0,0 +1,13 @@
+//myfirst.cpp--displays a messages
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+
+    cout << "You won't regret it!" << endl;
+    cin.get();
+    return 0;
+}
